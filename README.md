31c92aa6e30284137254c5ce185843104c160db0
Sergey Armodin <mak@arm1.ru>
2016-03-22 18:54:48 +0300

    - Init commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # jasmine-api-test
-Simple example of testing any API with jasmine
+Simple example of testing any API with [jasmine](http://jasmine.github.io)
+
+```bash
+npm install
+npm install jasmine -g
+npm install gulp -g
+gulp
+```
+
+Run ESLint and Jasmine tests via gulp:
+```bash
+gulp
+```
+
+Run just jasmine tests:
+```bash
+jasmine
+```
---
+++ spec/ApiSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const request = require("request");
+const base_url = "https://cloud-api.yandex.net/v1/";
+
+describe("GET /disk/", function() {
+    it("Unauthorized request. Successful test", function(done) {
+        request.get(base_url + 'disk/', function(error, response, body) {
+            expect(response.statusCode).toBe(401);
+            let responseObject = JSON.parse(body);
+            expect(responseObject.error).not.toBeUndefined();
+            done();
+        });
+    });
+
+    it("Unauthorized request. This test not gonna pass", function(done) {
+        request.get(base_url + 'disk/', function(error, response, body) {
+            expect(response.statusCode).toBe(404);
+            done();
+        });
+    });
+});
